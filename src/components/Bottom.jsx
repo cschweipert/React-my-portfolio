aef2351fd6ea8ee0f3e33016640cba17c2e70cb8
Christina Schweipert <c.schweipert@gmail.com>
2020-03-02 11:16:33 -0700

    change socialicon class

--- src/components/Bottom.jsx
+++ src/components/Bottom.jsx
@@ -2,14 +2,14 @@ import React from "react";
 
 function Bottom() {
     return <div className="bottom-container">
-    <a href="https://github.com/cschweipert">
-      <i className="fab fa-github fa-2x icon" title="icon link to github account"></i>
-    </a>
     <a href="https://www.linkedin.com/in/christina-schweipert-96b838162/">
-      <i className="fab fa-linkedin fa-2x icon" title="icon link to linkedin"></i>
+      <i className="fab fa-linkedin fa-2x socialicon" title="icon link to linkedin"></i>
+    </a>
+    <a href="https://github.com/cschweipert">
+      <i className="fab fa-github fa-2x socialicon" title="icon link to github account"></i>
     </a>
     <a href="https://instagram.com/missseagoat">
-      <i className="fab fa-instagram fa-2x icon" title="icon link to instagram"></i><br></br><br></br>
+      <i className="fab fa-instagram fa-2x socialicon" title="icon link to instagram"></i><br></br><br></br>
     </a>
     <a href="#top" className="upBtn">
       <i className="fas fa-angle-double-up fa-1x uparrow" title="link to top"></i>
